d459aa87a6e9d5ac11fceb90daba4d2004c87ee9
Ryan <collinsr729@yahoo.com>
2018-02-13 13:56:06 -0500

    Simple map point
    simple class call from main with an address

--- src/csc380/Main.java
+++ src/csc380/Main.java
@@ -1,5 +1,13 @@
 package csc380;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Welcome to the delivery service program.");
+		String a1 = JOptionPane.showInputDialog(null,"Enter an address:");	
+		Map m = new Map();
+		m.addPoint(a1);
+	}
 }
--- src/csc380/Map.java
+++ src/csc380/Map.java
@@ -2,4 +2,8 @@ package csc380;
 
 public class Map {
 
+	public void addPoint(String a1) {
+		
+	}
+
 }
