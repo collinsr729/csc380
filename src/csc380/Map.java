b1d66af55bf5f67b133740b6e908e6712002c7b4
Ryan <collinsr729@yahoo.com>
2018-02-15 00:06:37 -0500

    Added basic functionality
    A very barebones start to the code we may need to write for this project

--- src/csc380/Map.java
+++ src/csc380/Map.java
@@ -2,8 +2,26 @@ package csc380;
 
 public class Map {
 
+	public String[] addresses;
+	
+	public Map() {
+		addresses = new String[20];
+	}
+	
 	public void addPoint(String a1) {
+		for(int i = 0; i<addresses.length;i++)
+			if(addresses[i].equals(null)) {
+				addresses[i] = a1;
+				break;
+			}
+	}
+	
+	public String calculateRoute() {
 		
+		// Accesses a map API to get live updates on a route/ or find closest
+		// address and go in order from the next closest house to that one
+		
+		return "";
 	}
 
 }
