239ddf51794be94da9e4daad4d5fc1efbe9b74c5
sidorchukandrew <36050911+sidorchukandrew@users.noreply.github.com>
2018-02-15 13:14:34 -0500

    test

--- src/csc380/Main.java
+++ src/csc380/Main.java
@@ -1,5 +1,17 @@
 package csc380;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
+<<<<<<< HEAD
+	System.out.println("Hello");
+=======
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Welcome to the delivery service program.");
+		String a1 = JOptionPane.showInputDialog(null,"Enter an address:");	
+		Map m = new Map();
+		m.addPoint(a1);
+	}
+>>>>>>> b1d66af55bf5f67b133740b6e908e6712002c7b4
 }
