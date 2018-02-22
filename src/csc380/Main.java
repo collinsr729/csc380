4d743e2183e3a51c8cb041fe8f12a79c88c0609d
Andrew <36050911+sidorchukandrew@users.noreply.github.com>
2018-02-22 16:06:59 -0500

    Type in an address.

--- src/csc380/Main.java
+++ src/csc380/Main.java
@@ -1,22 +1,40 @@
 package csc380;
 
+import java.io.IOException;
+
 import javax.swing.JOptionPane;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.maps.*;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		GeoApiContext context = new GeoApiContext.Builder()
-			    .apiKey("AIzaSyCUcSoFBlKCqqxApVpprxj9CK6L7RrBhTU")
-			    .build();
-			GeocodingResult[] results =  GeocodingApi.geocode(context,
-			    "1600 Amphitheatre Parkway Mountain View, CA 94043").await();
-			Gson gson = new GsonBuilder().setPrettyPrinting().create();
-			System.out.println(gson.toJson(results[0].addressComponents));
 		
 		JOptionPane.showMessageDialog(null, "Welcome to the delivery service program.");
 		String a1 = JOptionPane.showInputDialog(null,"Enter an address:");
+		
+		
+		GeoApiContext context = new GeoApiContext.Builder()
+			    .apiKey("AIzaSyCUcSoFBlKCqqxApVpprxj9CK6L7RrBhTU")
+			    .build();
+			GeocodingResult[] results = null;
+			try {
+				results = GeocodingApi.geocode(context, a1).await();
+			} catch (ApiException | InterruptedException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			System.out.println(gson.toJson(results[0].addressComponents));
+		
+		/*JOptionPane.showMessageDialog(null, "Welcome to the delivery service program.");
+		String a1 = JOptionPane.showInputDialog(null,"Enter an address:");
 		Map m = new Map();
-		m.addPoint(a1);
+		m.addPoint(a1);*/
 	}
 
 	public void printDeliverySchedule()
