f912b6d0d067adbd80d087efd0116ededb67e365
Ryan <collinsr729@yahoo.com>
2018-02-22 13:11:31 -0500

    Maven Integration
    idk what this did

--- src/csc380/Main.java
+++ src/csc380/Main.java
@@ -3,29 +3,17 @@ package csc380;
 import javax.swing.JOptionPane;
 
 public class Main {
-
-//<<<<<<< HEAD
-//<<<<<<< HEAD
-	System.out.println("Hello");
-	System.out.println("This is a test");
-//=======
-//=======
-//>>>>>>> b1d66af55bf5f67b133740b6e908e6712002c7b4
+	
 	public static void main(String[] args) {
 		JOptionPane.showMessageDialog(null, "Welcome to the delivery service program.");
 		String a1 = JOptionPane.showInputDialog(null,"Enter an address:");
 		Map m = new Map();
 		m.addPoint(a1);
 	}
-/*<<<<<<< HEAD
->>>>>>> b1d66af55bf5f67b133740b6e908e6712002c7b4
-=======
->>>>>>> b1d66af55bf5f67b133740b6e908e6712002c7b4
-*/
 
 	public void printDeliverySchedule()
 	{
-		int orders;
+		int orders = 0;
 
 		System.out.println("Delivery Schedule:");
 		if(orders == 1)
