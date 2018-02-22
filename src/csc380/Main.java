8ed48e2804cc9a8c8263aa1fe93fa0db83c1b5b4
Andrew <36050911+sidorchukandrew@users.noreply.github.com>
2018-02-22 13:59:17 -0500

    GOOGLE API

--- src/csc380/Main.java
+++ src/csc380/Main.java
@@ -5,6 +5,14 @@ import javax.swing.JOptionPane;
 public class Main {
 	
 	public static void main(String[] args) {
+		GeoApiContext context = new GeoApiContext.Builder()
+			    .apiKey("AIzaSyCUcSoFBlKCqqxApVpprxj9CK6L7RrBhTU")
+			    .build();
+			GeocodingResult[] results =  GeocodingApi.geocode(context,
+			    "1600 Amphitheatre Parkway Mountain View, CA 94043").await();
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			System.out.println(gson.toJson(results[0].addressComponents));
+		
 		JOptionPane.showMessageDialog(null, "Welcome to the delivery service program.");
 		String a1 = JOptionPane.showInputDialog(null,"Enter an address:");
 		Map m = new Map();
@@ -33,6 +41,8 @@ public class Main {
 			System.out.println("Address two: ");
 			System.out.println("Address three: ");
 		}
+		
+		
 	}
 
 }
