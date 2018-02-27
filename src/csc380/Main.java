ba3359489711c52d8419c37a1dfffe912d2f3ff0
Andrew <36050911+sidorchukandrew@users.noreply.github.com>
2018-02-27 11:13:49 -0500

    Updated tryme

--- src/csc380/Main.java
+++ src/csc380/Main.java
@@ -8,11 +8,35 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.maps.*;
 import com.google.maps.errors.ApiException;
+import com.google.maps.model.Distance;
+import com.google.maps.model.DistanceMatrix;
+import com.google.maps.model.DistanceMatrixElement;
+import com.google.maps.model.DistanceMatrixRow;
 import com.google.maps.model.GeocodingResult;
 
 public class Main {
 	
 	public static void main(String[] args) {
+		
+		
+		/*GeoApiContext context = new GeoApiContext.Builder()
+			    .apiKey("AIzaSyCUcSoFBlKCqqxApVpprxj9CK6L7RrBhTU")
+			    .build();
+		
+		int numDestinations = 1;
+		
+		String[]originAddresses = new String[numDestinations];
+		String[]destinationAddresses = new String[numDestinations];
+		DistanceMatrixRow[]  rows = new DistanceMatrixRow[numDestinations];
+		
+		originAddresses[0] = "233 Slawson Drive Camillus, NY 13031";
+		destinationAddresses[0] = "237 Slawson Drive Camillus, NY 13031";
+
+		DistanceMatrix distance = new DistanceMatrix(originAddresses, destinationAddresses, rows);*/
+		
+		
+		
+		
 		
 		JOptionPane.showMessageDialog(null, "Welcome to the delivery service program.");
 		String a1 = JOptionPane.showInputDialog(null,"Enter an address:");
