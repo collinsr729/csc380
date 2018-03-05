6f9d76085b10c1e14276ded0d31fafe34e7b5e00
Andrew <36050911+sidorchukandrew@users.noreply.github.com>
2018-03-05 13:28:27 -0500

    Distance matrix has been implemented

--- src/csc380/Main.java
+++ src/csc380/Main.java
@@ -1,51 +1,42 @@
 package csc380;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JOptionPane;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.maps.*;
+import com.google.maps.DirectionsApi.RouteRestriction;
 import com.google.maps.errors.ApiException;
 import com.google.maps.model.Distance;
 import com.google.maps.model.DistanceMatrix;
-import com.google.maps.model.DistanceMatrixElement;
+import com.google.maps.model.DistanceMatrixElement; 
 import com.google.maps.model.DistanceMatrixRow;
 import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.TravelMode;
+
+import org.json.*;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		
 		
-		/*GeoApiContext context = new GeoApiContext.Builder()
-			    .apiKey("AIzaSyCUcSoFBlKCqqxApVpprxj9CK6L7RrBhTU")
-			    .build();
-		
-		int numDestinations = 1;
-		
-		String[]originAddresses = new String[numDestinations];
-		String[]destinationAddresses = new String[numDestinations];
-		DistanceMatrixRow[]  rows = new DistanceMatrixRow[numDestinations];
-		
-		originAddresses[0] = "233 Slawson Drive Camillus, NY 13031";
-		destinationAddresses[0] = "237 Slawson Drive Camillus, NY 13031";
-
-		DistanceMatrix distance = new DistanceMatrix(originAddresses, destinationAddresses, rows);*/
-		
-		
-		
-		
-		
 		JOptionPane.showMessageDialog(null, "Welcome to the delivery service program.");
-		String a1 = JOptionPane.showInputDialog(null,"Enter an address:");
+		String a[] = new String[3];
+		a[0] = JOptionPane.showInputDialog(null,"Enter an address:");
+		
+		Map map = new Map();
+		
+		map.DistanceCall(a);
 		
 		
-		GeoApiContext context = new GeoApiContext.Builder()
-			    .apiKey("AIzaSyCUcSoFBlKCqqxApVpprxj9CK6L7RrBhTU")
-			    .build();
-			GeocodingResult[] results = null;
+		
+		
+		/*GeocodingResult[] results = null;
 			try {
 				results = GeocodingApi.geocode(context, a1).await();
 			} catch (ApiException | InterruptedException | IOException e) {
@@ -53,12 +44,10 @@ public class Main {
 				e.printStackTrace();
 			}
 			Gson gson = new GsonBuilder().setPrettyPrinting().create();
-			System.out.println(gson.toJson(results[0].addressComponents));
-		
-		/*JOptionPane.showMessageDialog(null, "Welcome to the delivery service program.");
-		String a1 = JOptionPane.showInputDialog(null,"Enter an address:");
-		Map m = new Map();
-		m.addPoint(a1);*/
+			System.out.println(gson.toJson(results[0].geometry));
+			
+			JSONObject obj = new JSONObject(gson.toJson(results[0].geometry));
+			System.out.println(obj);*/
 	}
 
 	public void printDeliverySchedule()
--- src/csc380/Map.java
+++ src/csc380/Map.java
@@ -1,19 +1,26 @@
 package csc380;
 
+
+import org.json.simple.JSONObject;
+
+//import org.json.JSONObject;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.maps.DistanceMatrixApi;
+import com.google.maps.DistanceMatrixApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DistanceMatrix;
+import com.google.maps.model.TravelMode;
+
 public class Map {
 
 	public String[] addresses;
 	
 	public Map() {
-		addresses = new String[20];
-	}
-	
-	public void addPoint(String a1) {
-		for(int i = 0; i<addresses.length;i++)
-			if(addresses[i].equals(null)) {
-				addresses[i] = a1;
-				break;
-			}
+
 	}
 	
 	public String calculateRoute() {
@@ -22,6 +29,56 @@ public class Map {
 		// address and go in order from the next closest house to that one
 		
 		return "";
+	}
+	
+	public void DistanceCall(String addresses[])
+	{
+		final String HOME_BASE = "7060 NY104";
+		
+		String address1, address2, address3;
+		String object;
+		
+		address1 = addresses[0];
+		address2 = addresses[1];
+		address3 = addresses[2];
+		
+		
+		final String GEO_API_KEY = "AIzaSyCUcSoFBlKCqqxApVpprxj9CK6L7RrBhTU";
+		GeoApiContext context = new GeoApiContext.Builder()
+			    .apiKey(GEO_API_KEY)
+			    .build();
+		DistanceMatrix trix = null;
+		
+		if(address2 == null)
+		{
+			try
+			{
+				DistanceMatrixApiRequest req = DistanceMatrixApi.newRequest(context); 
+		         trix = req.origins(HOME_BASE)
+		                .destinations(address1)
+		                .mode(TravelMode.DRIVING)
+		                //.avoid(RouteRestriction.TOLLS)
+		                .language("en-EN")
+		                .await();
+			}
+			
+			catch(ApiException e){
+		        
+		    } catch(Exception e){
+		        System.out.println(e.getMessage());
+		    }   
+			
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			System.out.println(gson.toJson(trix.rows[0]));		
+			
+			object = gson.toJson(trix);
+			
+			
+			//JsonObject obj = gson.toJson(trix);
+			
+
+			
+		}
 	}
 
 }
