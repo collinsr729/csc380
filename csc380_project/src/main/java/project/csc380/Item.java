05014fe920996db01a847cf890a67795be30076b
Andrew <36050911+sidorchukandrew@users.noreply.github.com>
2018-03-08 23:50:20 -0500

    Create an actual maven project out of our original
    There are two copies of our project right now.

---
+++ csc380_project/src/main/java/project/csc380/Item.java
@@ -0,0 +1,23 @@
+package project.csc380;
+
+public class Item {
+	String foodName;
+	int prepTime;
+	
+	public Item(String foodName, int prepTime)
+	{
+		this.foodName = foodName;
+		this.prepTime = prepTime;
+	}
+	
+	public String getFoodName()
+	{
+		return foodName;
+	}
+	
+	public int getPrepTime()
+	{
+		return prepTime;
+	}
+
+}
