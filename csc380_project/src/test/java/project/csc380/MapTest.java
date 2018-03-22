59cb4a26779ec7c73a3bee6de4b67c5a99768fc5
Andrew <36050911+sidorchukandrew@users.noreply.github.com>
2018-03-22 12:44:22 -0400

    Added some GUI classes

---
+++ csc380_project/src/test/java/project/csc380/MapTest.java
@@ -0,0 +1,22 @@
+package project.csc380;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class MapTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	public void DistanceCallTest()
+	{
+		Map instance = new Map();
+		String expected;
+	}
+
+}
